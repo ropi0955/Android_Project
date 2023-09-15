1d7bf3c7c0b91c43923434a02e2a32dd7e970c64
ropi0955 <szemanyik.gergely@gmail.com>
2023-09-15 18:05:27 +0200

    go to the profil button and profil page create

--- app/src/main/java/hu/nje/tienda/MainActivity.java
+++ app/src/main/java/hu/nje/tienda/MainActivity.java
@@ -2,19 +2,25 @@ package hu.nje.tienda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 private Button buttonNewProduct;
+private ImageButton profileButton;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         buttonNewProduct = findViewById(R.id.buttonNewProduct);
+        profileButton = findViewById(R.id.profileButton);
+
 
 
 
@@ -27,6 +33,15 @@ private Button buttonNewProduct;
             openNewProduct();
             }
         });
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {openProfileActivity(); }
+        });
+    }
+
+    private void openProfileActivity() {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivities(new Intent[]{intent});
     }
 
     private void openNewProduct() {

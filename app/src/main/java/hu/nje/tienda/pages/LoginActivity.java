a0d943459f22575ed3da84caee76eb77fe01fb75
zsxlesz <136483423+zsxlesz@users.noreply.github.com>
2023-09-15 21:40:34 +0200

    initial commit
    Bejelentkezés és regisztráció frontend kész
    Adatbázis elkezdve

---
+++ app/src/main/java/hu/nje/tienda/pages/LoginActivity.java
@@ -0,0 +1,14 @@
+package hu.nje.tienda.pages;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import hu.nje.tienda.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

69122d4cf2a18157167f9d83e9aa3dfa79a47acc
vagnerbence <vagner.bence@gmail.com>
2023-09-15 17:12:46 +0200

    setting colors NewProduct->MainActivity

--- app/src/main/java/hu/nje/tienda/NewProduct.java
+++ app/src/main/java/hu/nje/tienda/NewProduct.java
@@ -2,13 +2,31 @@ package hu.nje.tienda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class NewProduct extends AppCompatActivity {
-
+private FloatingActionButton backMainActivityButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_product);
+
+        backMainActivityButton = findViewById(R.id.backMainActivityButton);
+        backMainActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    backMainActivity();
+            }
+        });
+
+    }
+
+    private void backMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivities(new Intent[]{intent});
     }
 }

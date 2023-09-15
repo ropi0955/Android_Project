ad120ef62d5f31e4cc425656e5c831dc26d22d2d
ropi0955 <szemanyik.gergely@gmail.com>
2023-09-16 00:16:25 +0200

    profile page and pw change page added --some button not work

--- app/src/main/java/hu/nje/tienda/ProfileActivity.java
+++ app/src/main/java/hu/nje/tienda/ProfileActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import hu.nje.tienda.pages.ChangePasswordActivity;
 
 public class ProfileActivity extends AppCompatActivity {
     private FloatingActionButton backMainActivityButton2;
     private FloatingActionButton modifyProfileButton;//valahogy az összes textboxot konténerbe kéne tenni és akkor egybe szerkeszhetővé lehetne tenni..
+    private Button changePasswordbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +36,13 @@ public class ProfileActivity extends AppCompatActivity {
                 modifyProfileButton();
             }
         });
+        changePasswordbutton = findViewById(R.id.changePasswordbutton);
+        changePasswordbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changePasswordbutton();
+            }
+        });
     }
     private void backMainActivity2() {
         Intent intent = new Intent(this, MainActivity.class);
@@ -41,6 +52,10 @@ public class ProfileActivity extends AppCompatActivity {
     //ez itt nem tudom kell-e?!
     private void modifyProfileButton() {
         Intent intent = new Intent(this, ProfileActivity.class);
+        startActivities(new Intent[]{intent});
+    }
+    private void changePasswordbutton() {
+        Intent intent = new Intent(this, ChangePasswordActivity.class);
         startActivities(new Intent[]{intent});
     }
 }

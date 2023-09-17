6562c717a2a8eb27deaa6fe5ca6d7b07dd37402b
email <szbfrnc7@gmail.com>
2023-09-17 20:54:33 +0200

    creating of signup and data security basic pages without

---
+++ app/src/main/java/hu/nje/tienda/pages/DataSecurity.java
@@ -0,0 +1,16 @@
+package hu.nje.tienda.pages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import hu.nje.tienda.R;
+
+public class DataSecurity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_security);
+    }
+}

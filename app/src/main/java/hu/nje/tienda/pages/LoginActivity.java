bc080ad64461ee1cab81d93ea3126b27b52cd673
zsxlesz <136483423+zsxlesz@users.noreply.github.com>
2023-09-18 20:08:27 +0200

    Form
    Regisztráció és bejelentkezés form adatbázisba insertelése és ellenőrzések

--- app/src/main/java/hu/nje/tienda/pages/LoginActivity.java
+++ app/src/main/java/hu/nje/tienda/pages/LoginActivity.java
@@ -2,13 +2,50 @@ package hu.nje.tienda.pages;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import hu.nje.tienda.R;
+import hu.nje.tienda.database.DatabaseHelper;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+    EditText email, password;
+    Button signin;
+    DatabaseHelper MyDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.login_email);
+        password = findViewById(R.id.login_password);
+        signin = findViewById(R.id.login_button);
+        MyDatabase = new DatabaseHelper(this);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String user_email = email.getText().toString();
+                String user_password = password.getText().toString();
+
+                if (TextUtils.isEmpty(user_email) || TextUtils.isEmpty(user_password)){
+                    Toast.makeText(LoginActivity.this, "Nem töltött ki minden mezőt", Toast.LENGTH_SHORT).show();
+                }else{
+                    Boolean checkuseremailpass = MyDatabase.checkEmailAndPassword(user_email, user_password);
+                    if (checkuseremailpass){
+                        Toast.makeText(LoginActivity.this, "Sikeres bejelentkezés", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Sikertelen bejelentkezés", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
     }
 }

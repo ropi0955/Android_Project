129656088608c96e64ec4637d71ac6e91bfd0ad2
email <szbfrnc7@gmail.com>
2023-09-19 20:56:25 +0200

    new table view on "new_order java and xml". and calculation functions to price,quantity, and total

--- app/src/main/java/hu/nje/tienda/pages/NewOrder.java
+++ app/src/main/java/hu/nje/tienda/pages/NewOrder.java
@@ -7,36 +7,129 @@ import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import hu.nje.tienda.R;
 
 public class NewOrder extends AppCompatActivity /*implements AdapterView.OnItemSelectedListener*/ {
-private Spinner spinner2;
+
+    private ArrayList<String> data = new ArrayList<String>();
+    private ArrayList<String> data1 = new ArrayList<String>();
+    private ArrayList<String> data2 = new ArrayList<String>();
+    private ArrayList<String> data3 = new ArrayList<String>();
+
+    private TableLayout table;
+
+    EditText ed1, ed2, ed3, ed4, ed5, ed6;
+    Button b1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_order);
 
-        spinner2 = findViewById(R.id.spinner2);
 
-        initSpinner(R.array.howmanypieces,spinner2);
-    }
-       private void initSpinner(int resId, Spinner spinner)
-        {
-        ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(this,R.array.howmanypieces, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-    }
+        View spinner2 = findViewById(R.id.spinner2);
+        ed2 = findViewById(R.id.ed2);
+        ed3 = findViewById(R.id.ed3);
 
-   /* @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
-        String text= parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(),text, Toast.LENGTH_SHORT).show();
+
+        b1 = findViewById(R.id.btn1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add();
+            }
+        });
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-}*/
+    public class NewOrder2 extends AppCompatActivity {
+        private Spinner spinner2;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_new_order);
+
+            spinner2 = findViewById(R.id.spinner2);
+
+            initSpinner(R.array.howmanypieces, spinner2);
+        }
+
+        ;
+
+        private void initSpinner(int resId, Spinner spinner) {
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, resId, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(adapter);
+        }
     }
+
+
+    public void add() {
+
+        int tot;
+
+        String prodname = ed1.getText().toString();
+        int price = Integer.parseInt(ed2.getText().toString());
+        int qty = Integer.parseInt(ed3.getText().toString());
+        tot = price * qty;
+
+        data.add(prodname);
+        data1.add(String.valueOf(price));
+        data2.add(String.valueOf(qty));
+        data3.add(String.valueOf(tot));
+
+        TableLayout table = (TableLayout) findViewById(R.id.tb1);
+
+        TableRow row = new TableRow(this);
+        TextView t1 = new TextView(this);
+        TextView t2 = new TextView(this);
+        TextView t3 = new TextView(this);
+        TextView t4 = new TextView(this);
+
+        String total;
+
+        int sum = 0;
+
+        for (int i = 0; i < data.size(); i++) {
+            String pname = data.get(i);
+            String prc = data1.get(i);
+            String qtyy = data2.get(i);
+
+            total = data3.get(i);
+
+            t1.setText(pname);
+            t2.setText(prc);
+            t3.setText(qty);
+            t4.setText(total);
+
+            sum = sum + Integer.parseInt(data3.get(i).toString());
+
+        }
+
+        row.addView(t1);
+        row.addView(t2);
+        row.addView(t3);
+        row.addView(t4);
+        table.addView(row);
+
+        ed4.setText(String.valueOf(sum));
+        ed1.setText("");
+        ed2.setText("");
+        ed3.setText("");
+        ed1.requestFocus();
+
+
+    }
+}
+
+

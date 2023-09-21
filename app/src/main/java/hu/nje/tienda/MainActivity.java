80377c4cde1b7e6199862e245440c60dbd91e3fe
ropi0955 <szemanyik.gergely@gmail.com>
2023-09-21 21:46:33 +0200

    bugfix

--- app/src/main/java/hu/nje/tienda/MainActivity.java
+++ app/src/main/java/hu/nje/tienda/MainActivity.java
@@ -1,13 +1,13 @@
 package hu.nje.tienda;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import hu.nje.tienda.pages.MarketActivity;
 import hu.nje.tienda.pages.NewOrder;

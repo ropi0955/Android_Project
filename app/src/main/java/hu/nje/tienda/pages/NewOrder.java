17485714e82d16d04bcfd75365b421ce05b06d4d
email <szbfrnc7@gmail.com>
2023-11-03 19:40:44 +0100

    new order bug fixing

--- app/src/main/java/hu/nje/tienda/pages/NewOrder.java
+++ app/src/main/java/hu/nje/tienda/pages/NewOrder.java
@@ -4,23 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Adapter;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import hu.nje.tienda.R;
 
-public class NewOrder extends AppCompatActivity /*implements AdapterView.OnItemSelectedListener*/ {
+public class NewOrder extends AppCompatActivity {
 
     private ArrayList<String> data = new ArrayList<String>();
     private ArrayList<String> data1 = new ArrayList<String>();
@@ -29,23 +23,23 @@ public class NewOrder extends AppCompatActivity /*implements AdapterView.OnItemS
 
     private TableLayout table;
 
-    EditText ed1, ed2, ed3, ed4;
+    EditText ed1, ed2, ed3;
     Button b1;
+    TextView totalSum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_order);
 
-
         ed1 = findViewById(R.id.ed1);
         ed2 = findViewById(R.id.ed2);
         ed3 = findViewById(R.id.ed3);
 
-
         b1 = findViewById(R.id.btn1);
-        b1.setOnClickListener(new View.OnClickListener() {
+        totalSum = findViewById(R.id.t4);
 
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 add();
@@ -53,24 +47,19 @@ public class NewOrder extends AppCompatActivity /*implements AdapterView.OnItemS
         });
     }
 
-
-
-    public void add()
-    {
-
-        int tot;
-
+    public void add() {
         String prodname = ed1.getText().toString();
         int price = Integer.parseInt(ed2.getText().toString());
         int qty = Integer.parseInt(ed3.getText().toString());
-        tot = price * qty;
+        int tot = price * qty;
 
         data.add(prodname);
         data1.add(String.valueOf(price));
         data2.add(String.valueOf(qty));
         data3.add(String.valueOf(tot));
 
-        TableLayout table = (TableLayout) findViewById(R.id.tb1);
+        TableLayout table = findViewById(R.id.tb1);
+
 
         TableRow row = new TableRow(this);
         TextView t1 = new TextView(this);
@@ -78,41 +67,30 @@ public class NewOrder extends AppCompatActivity /*implements AdapterView.OnItemS
         TextView t3 = new TextView(this);
         TextView t4 = new TextView(this);
 
-        String total;
-
-        int sum = 0;
-
-        for (int i = 0; i < data.size(); i++) {
-            String pname = data.get(i);
-            String prc = data1.get(i);
-            String qtyy = data2.get(i);
-
-            total = data3.get(i);
-
-            t1.setText(pname);
-            t2.setText(prc);
-            t3.setText(qty);
-            t4.setText(total);
-
-            sum = sum + Integer.parseInt(data3.get(i).toString());
-
-        }
+        t1.setText(prodname);
+        t2.setText(String.valueOf(price));
+        t3.setText(String.valueOf(qty));
+        t4.setText(String.valueOf(tot));
 
         row.addView(t1);
         row.addView(t2);
         row.addView(t3);
         row.addView(t4);
+
         table.addView(row);
 
-        ed4.setText(String.valueOf(sum));
+        int sum = 0;
+
+        for (int i = 0; i < data3.size(); i++) {
+            sum += Integer.parseInt(data3.get(i));
+        }
+
+        totalSum.setText("Total: " + sum);
+
+
         ed1.setText("");
         ed2.setText("");
         ed3.setText("");
         ed1.requestFocus();
-
-
-
     }
 }
-
-

d3a8f419fddf56e32d0f4cee148bb7b08e3b70a1
ropi0955 <szemanyik.gergely@gmail.com>
2023-11-05 18:38:22 +0100

    profile o page xml and java modifications

--- app/src/main/java/hu/nje/tienda/ProfileActivity.java
+++ app/src/main/java/hu/nje/tienda/ProfileActivity.java
@@ -1,31 +1,45 @@
 package hu.nje.tienda;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import hu.nje.tienda.database.DatabaseHelper;
+import hu.nje.tienda.services.UserDatasService;
 
 public class ProfileActivity extends AppCompatActivity {
-    private FloatingActionButton backMainActivityButton2;
+    private DatabaseHelper databaseHelper;
+    private UserDatasService userDataService;
+
+    private TextView textFullName;
+    private EditText textEmailAddress;
+    private EditText textPhone;
+    private EditText textPostalAddress;
+    private EditText textPostCode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        backMainActivityButton2 = findViewById(R.id.backMainActivityButton2);
-        backMainActivityButton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                backMainActivity2();
-            }
-        });
-    }
-    private void backMainActivity2() {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivities(new Intent[]{intent});
-    }
+        databaseHelper = new DatabaseHelper(this);
+        userDataService = new UserDatasService();
 
+        textFullName = findViewById(R.id.textFullName);
+        textEmailAddress = findViewById(R.id.textEmailAddress);
+        textPhone = findViewById(R.id.textPhone);
+        textPostalAddress = findViewById(R.id.textPostalAddress);
+        textPostCode = findViewById(R.id.textPostCode);
+
+        String userEmail = userDataService.getUser_email_address();
+        databaseHelper.setDatas(userEmail);
+
+        textFullName.setText(userDataService.getUser_first_name() + " " + userDataService.getUser_last_name());
+        textEmailAddress.setText(userEmail);
+        textPhone.setText(userDataService.getPhone_number());
+        textPostalAddress.setText(userDataService.getUser_city() + ", " + userDataService.getUser_street() + ", " + userDataService.getUser_street_number());
+        textPostCode.setText("irányítószám"); // Itt az irányítószámot kell beállítani a megfelelő adat alapján
+    }
 }
